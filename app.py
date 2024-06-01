d784ffe4c1ea658a90df5c4e7af444961f3d5c68
Aman Sharma <153918062+aman-sharma-dev@users.noreply.github.com>
2024-06-01 16:32:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import ssl
 from urllib.parse import quote 
  
 openai.api_key = 'your API code'
@@ -19,7 +18,7 @@ limiter = Limiter(
     storage_uri="memory://"
 ) 
  
-password = 'Bareilly@123'
+password = 'password here'
 encoded_password = quote(password)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://root:{encoded_password}@localhost/users'
@@ -110,7 +109,7 @@ def generate_answer(text):
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
-                {"role": "system", "content": "your name is luna you are a sales assistent of salesharbor, salesharbor website is salesharbor.io and salesharbor is formed in 2023, generate brief answers if The user expresses a desire to schedule a call with the sales team. Luna assures the user that a team member will contact them to arrange the call. When inquiring about pricing, Luna mentions that a team member will provide detailed information during a scheduled call. In response to the user's question about services, Luna outlines two offerings: Appointment Setting and Sales Consultancy, detailing the features of the Appointment Setting service. The user asks about the timeline for seeing results, and Luna explains the process, emphasizing that results start from Month 1. Regarding team assignment, Luna assures the user of a dedicated account manager and describes the internal collaboration involving data miners, copywriters, and outreach specialists. Luna also explains the monthly email volume will be ranging from 8000 to 22000, dependent on the chosen plan, and mentions a forthcoming call to discuss these plans. The user inquires about the commitment period, and Luna offers a risk-free trial of 3 months with a pay-per-lead option, transitioning to a monthly retainer afterward. Luna elaborates on lead sourcing, detailing the use of platforms like LinkedIn and ZoomInfo and the meticulous validation process, emphasizing the delivery of tailored, high-quality leads aligned with the user's goals.The headquarters of Salesharbor is in Amsterdam in the Netherlands.If user ask about SalesHarbor then Luna should respond SalesHarbor, with over two decades of expertise in sales and lead generation, excels in connecting businesses with ideal clients for strategic growth. Founded on the fusion of art and science, our commitment to excellence is built on a solid 20-year foundation. If a user asks a question that is not related to Sales Harbor or sales and marketing,If user ask about SalesHarbor then reply, Luna will respond by asking, 'Do you have any specific questions about SalesHarbor?"},
+                {"role": "system", "content": "Define you chat bot features and roles and how should it act as"},
                 {"role": "user", "content": chunk}
             ]
         )
@@ -120,7 +119,7 @@ def generate_answer(text):
 def send_email(user_ip, user_input, name, email):
     subject = f'User Input Log for {name} - Email: {email}'
     body = f"User IP: {user_ip}\nUser Input: {user_input}\nTimestamp: {datetime.utcnow()}\nUser Name: {name}\nUser Email: {email}"
-    recipients = ['amansharma22102004@gmail.com','mona.juneja@salesharbor.io']
+    recipients = ['user@email.com']
 
     msg = Message(subject=subject, body=body, recipients=recipients)
 
@@ -129,11 +128,6 @@ def send_email(user_ip, user_input, name, email):
     except Exception as e:
         print(f"Error sending email: {e}")
 
-ssl_certificate_path = '/etc/nginx/ssl-certificates/bot1.salesharbor.io.crt'
-ssl_private_key_path = '/etc/nginx/ssl-certificates/bot1.salesharbor.io.key'
-
-ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-ssl_context.load_cert_chain(certfile=ssl_certificate_path, keyfile=ssl_private_key_path)
 
 if __name__ == '__main__':
     with app.app_context():
@@ -141,6 +135,6 @@ if __name__ == '__main__':
 
     try:
         app.secret_key = 'zZIZC6ZUxbqQWE8qIGSf'
-        app.run(host='0.0.0.0', port='443', debug=True, ssl_context=ssl_context)
+        app.run(host='0.0.0.0', port='443', debug=True)
     except Exception as e:
         print(f"Error starting the application: {e}")
